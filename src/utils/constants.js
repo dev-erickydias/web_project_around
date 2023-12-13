80506e6bc077a093ec4629cd5038d5d10e2b6434
Ericky Dias <erickyhenriquesd@gmail.com>
2023-12-13 23:49:15 +0000

    add constants no file de contantes

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,26 +1,49 @@
 export const initialCards = [
-    {
-      name: "Vale de Yosemite",
-      link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_yosemite.jpg",
-    },
-      {
-      name: "Lago Louise",
-      link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lake-louise.jpg",
-    },
-    {
-      name: "Montanhas Carecas",
-      link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_bald-mountains.jpg",
-    },
-    {
-      name: "Latemar",
-      link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_latemar.jpg",
-    },
-    {
-      name: "Vanois National",
-      link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_vanoise.jpg",
-    },
-    {
-      name: "Lago di Braies",
-      link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lago.jpg",
-    },
-  ];
+  {
+    name: "Vale de Yosemite",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_yosemite.jpg",
+  },
+  {
+    name: "Lago Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lake-louise.jpg",
+  },
+  {
+    name: "Montanhas Carecas",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_latemar.jpg",
+  },
+  {
+    name: "Vanois National",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lago.jpg",
+  },
+];
+
+export const profileButton = document.querySelector(".profile__button");
+export const popupUserForm = document.querySelector("#popup-user-form");
+export const profileTitle = document.querySelector(".profile__title");
+export const profileSubtitle = document.querySelector(".profile__subtitle");
+export const inputNome = document.querySelector(".popup__form-name");
+export const inputJob = document.querySelector(".popup__form-job");
+export const formClose = document.querySelector(".popup__form-close");
+export const submitForm = document.querySelector("#button__submit");
+export const addButton = document.querySelector(".profile__button-add");
+export const popupCardForm = document.querySelector("#popup-card-form");
+export const addClose = popupCardForm.querySelector("#close-add");
+export const addInputName = popupCardForm.querySelector("#input__title");
+export const addInputImage = popupCardForm.querySelector("#input__image");
+export const addSubmit = popupCardForm.querySelector("#add__submit");
+export const cards = document.querySelector(".cards");
+export const popupImage = document.querySelector("#popupcard");
+export const imgClose = popupImage.querySelector(".close-image");
+export const overlay = document.querySelector(".overlay");
+export const overlayAdd = document.querySelector("#overlay-add");
+export const overlayImage = document.querySelector("#overlay-image");
+export const addFormFirst = document.querySelector("#first");
+export const editForm = document.querySelector(".popup__form-edit");
